2b8a5487c717cf088b04f398c37ad2e2c9ff9f51
MyGuest <MyGuest@LAPTOP-BP403IAL>
2017-09-27 18:35:07 -0500

    I believe I have finished the lab portion for lab 4.

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -1,5 +1,5 @@
 /*
- * The following code needs a lot of TLC. So give it some!
+ * The following code needs firstString lot of TLC. So give it some!
  *
  * 1. Fix all checkstyle errors
  * 2. Determine what it does (it's going to be tough before you finish #1)
@@ -7,20 +7,42 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
+/**
+ *  This class contains a method which returns the largest similar
+ *  set of characters shared between the two strings.
+ */
+public class ReclamationProject {
+    /**
+     *
+     * @param firstString - a string input.
+     * @param secondString - another string input, which will be compared to firstString
+     * @return the largest similar set of characters shared between the two strings.
+     */
+    public static String largestSharedChar(final String firstString, final String secondString) {
+        String reclaim = ""; //this will be the returned set of shared characters
+        //this for loop will go through all possible and valid indices
+        //of the two strings and also possible lengths of characters
+        //that they can share to determine the largest set of characters that the two strings share.
+        for (int firstStart = 0; firstStart < firstString.length(); firstStart++) {
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+            for (int num = firstString.length() - firstStart; num > 0; num--) {
+                for (int secondStart = 0;
+                        secondStart < secondString.length() - num; secondStart++) {
+                    if (firstString.regionMatches(firstStart, secondString, secondStart, num)
+                            && num > reclaim.length()) {
+                        reclaim = firstString.substring(firstStart, firstStart + num);
+                    }
+
+                }
+            }
+        }
+        return reclaim;
+    }
+    /**
+     * runs the method largestSharedChar.
+     * @param args - is a part of the call to the main method
+     */
+    public static void main(final String[] args) {
+        System.out.println(largestSharedChar("The man loves her", "Theads loved the man."));
+    }
 }
